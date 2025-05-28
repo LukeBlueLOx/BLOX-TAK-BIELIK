d0da1e45e299fa20781d21de8f7d1addae65c9a1
luke_blue_lox <luke.blue.lox@gmail.com>
2025-05-28 15:07:47 +0200

    2025-05-28-5

--- extract_fragment_text.py
+++ extract_fragment_text.py
@@ -13,16 +13,10 @@ def extract_pages(pdf_path, start_page, end_page):
 
 # Załóżmy, że artykuł 13 jest na stronie 3
 # Możesz podać zakres np. od strony 2 do 4, żeby dać modelowi trochę więcej kontekstu
-strony_do_ekstrakcji_start = 2 # Strona 2 PDF to indeks 1
-strony_do_ekstrakcji_end = 4   # Strona 4 PDF to indeks 3
+strony_do_ekstrakcji_start = 3 # Strona 2 PDF to indeks 1
+strony_do_ekstrakcji_end = 3   # Strona 4 PDF to indeks 3
 
 konstytucja_fragment_text = extract_pages("KONSTYTUCJA_RP.pdf", strony_do_ekstrakcji_start, strony_do_ekstrakcji_end)
 
 with open("konstytucja_fragment.txt", "w") as f:
     f.write(konstytucja_fragment_text)
-
-# Jeśli nadal potrzebujesz całego sprzeciwu:
-with pdfplumber.open("SPRZECIW_2025-03-03.pdf") as pdf:
-    sprzeciw_text = "".join(page.extract_text() for page in pdf.pages)
-with open("sprzeciw.txt", "w") as f:
-    f.write(sprzeciw_text)
