88fa31e35962c32ddeae1a3667c99a9495622100
luke_blue_lox <luke.blue.lox@gmail.com>
2025-05-28 15:50:45 +0200

    2025-05-28-6

--- extract_fragment_text.py
+++ extract_fragment_text.py
@@ -4,17 +4,14 @@ import pdfplumber
 def extract_pages(pdf_path, start_page, end_page):
     text = ""
     with pdfplumber.open(pdf_path) as pdf:
-        # Pamiętaj, że w Pythonie indeksowanie zaczyna się od 0
-        # Więc strona 3 to indeks 2
         for i in range(start_page - 1, end_page):
             if i < len(pdf.pages): # Sprawdź, czy strona istnieje
                 text += pdf.pages[i].extract_text() + "\n" # Dodaj znak nowej linii po każdej stronie
     return text
 
 # Załóżmy, że artykuł 13 jest na stronie 3
-# Możesz podać zakres np. od strony 2 do 4, żeby dać modelowi trochę więcej kontekstu
-strony_do_ekstrakcji_start = 3 # Strona 2 PDF to indeks 1
-strony_do_ekstrakcji_end = 3   # Strona 4 PDF to indeks 3
+strony_do_ekstrakcji_start = 3
+strony_do_ekstrakcji_end = 3
 
 konstytucja_fragment_text = extract_pages("KONSTYTUCJA_RP.pdf", strony_do_ekstrakcji_start, strony_do_ekstrakcji_end)
 
